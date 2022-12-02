afad9c5b3083c4569a1cecb063a87dd676aceb08
Nick Warters <nick.warters@gmail.com>
2022-12-02 19:56:28 +0000

    Day 4 Part 2

---
+++ src/04/part2.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs'
+import { createHash } from 'crypto'
+
+const tests: [string, any][] = [
+	[`abcdef`, 609043],
+	['pqrstuv', 1048970],
+]
+
+tests.forEach(([testData, expected]) => {
+	const result = solution(testData)
+	console.log(
+		`Example Input Solution - Expected: ${expected}, Got: ${result}, ${
+			result === expected ? 'PASS' : 'FAIL'
+		}`
+	)
+})
+
+console.log(
+	'Full Input Solution',
+	solution(readFileSync('./input.txt', { encoding: 'utf-8' }))
+)
+function md5(input: string) {
+	return createHash('md5').update(input).digest('hex')
+}
+
+function checkMatch(input: string) {
+	return input.startsWith('000000')
+}
+function solution(input: string): any {
+	let count = 0
+	while (!checkMatch(md5(`${input}${count}`))) count++
+
+	return count
+}
